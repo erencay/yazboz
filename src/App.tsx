d4e509de8ee76ea092e2ed4afb8704257f6e07e3
Eren Çay <ogealter@gmail.com>
2020-02-22 14:28:27 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [turn, setTurn] = React.useState(0);
+    const [players, setPlayers] = React.useState(["Esat", "Ceren", "Eren"]);
+    const [scores, setScores] = React.useState<number[][]>([]);
+
+    function addScores() {
+        const newScores = players.map((_, i) => {
+            const el = document.getElementById("input" + i) as HTMLInputElement;
+            const val = parseInt(el.value) || 0;
+            el.value = "";
+
+            return val;
+        });
+
+        setScores([newScores, ...scores]);
+        setTurn((turn + 1) % players.length);
+    }
+
+    const totalScores: number[] = [];
+    players.forEach((p, idx) => {
+        totalScores[idx] = totalScores[idx] || 0;
+
+        scores.forEach(subScores => {
+            totalScores[idx] += subScores[idx];
+        })
+    });
+
+    return (
+        <div className="App">
+            <table>
+                <thead>
+                    <tr>
+                        {players.map((player, idx) => {
+                            return <th key={player}>
+                                <input
+                                    id={"input" + idx}
+                                    className={idx === turn ? "active" : ""}
+                                    placeholder={player}
+                                />
+                            </th>
+                        })}
+
+                        <th className={"action"}>
+                            <div className={"btn"} onClick={addScores}>
+                                <span>+</span>
+                            </div>
+                        </th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        {totalScores.map((s, i) => {
+                            return <td key={i} className={"total-score"}>{s}</td>
+                        })}
+                    </tr>
+
+                    {scores.map((subscores, idx) => {
+                        return <tr key={idx}>
+                            {subscores.map((score, idx) => (
+                                <td key={idx}>{score}</td>
+                            ))}
+                        </tr>
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default App;

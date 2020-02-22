26be656527c950d25667973c951e6e235b3ebe74
Eren Çay <ogealter@gmail.com>
2020-02-22 14:30:53 +0300

    up

---
+++ build/precache-manifest.265eaad2a251892a7de4344a9479a341.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbcaf02d8ba2089b3bbb06a0684016f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a77e68495795471689d",
+    "url": "/static/css/main.d998d1b9.chunk.css"
+  },
+  {
+    "revision": "4d9f1b59ef0d71d0da42",
+    "url": "/static/js/2.95d81b9c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.95d81b9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a77e68495795471689d",
+    "url": "/static/js/main.8e2777a6.chunk.js"
+  },
+  {
+    "revision": "0a2697f6dadef3e8cdd1",
+    "url": "/static/js/runtime-main.7741f4de.js"
+  }
+]);
